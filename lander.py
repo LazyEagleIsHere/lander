6a373362f27d81f986929cc8ff8c308fcc6fa0fc
h_psi_t <137602805+LazyEagleIsHere@users.noreply.github.com>
2025-04-28 22:09:15 +0800

    Add files via upload

---
+++ lander.py
@@ -0,0 +1,150 @@
+import pygame
+import sys
+import time
+from text import *
+
+pygame.init()
+
+def main():
+
+  display_info = pygame.display.Info()
+  width, height = display_info.current_w, display_info.current_h
+  fps = 60
+  clock = pygame.time.Clock()
+
+  black = (0, 0, 0)
+  white = (255, 255, 255)
+  red = (255, 0, 0)
+  gray = (200, 200, 200)
+  orange = (255, 165, 0)
+  light_blue = (173, 116, 233)
+
+  screen = pygame.display.set_mode((width, height))
+  pygame.display.set_caption('E')
+  font = pygame.font.Font(None, 36)
+
+  spaceship_pos = [10, 10]
+  gravity = 3.0
+  a = 6.0
+  v1 = -0.1
+  v2 = 0.0
+
+  def draw(text, font_size, y_position):
+    font = pygame.font.Font(None, font_size)
+    text_render = font.render(text, True, gray)
+    text_rect = text_render.get_rect(center=(width // 2, y_position))
+    screen.blit(text_render, text_rect)
+
+  def start_screen():
+    screen.fill(black)
+    starter = True
+    while starter:
+      draw("E", 100, height // 4)
+      
+      start = pygame.Rect(width // 2 - 200, height // 2 + 50, 400, 100)
+      lead = pygame.Rect(width // 2 - 200, height // 2 + 200, 400, 100)
+      
+      mouse_pos = pygame.mouse.get_pos()
+
+      
+      if start.collidepoint(mouse_pos):
+        write(screen, start, "Start", 65, "black", "gray69", 10)
+      else:
+        write(screen, start, "Start", 65, "black", "white", 10)
+      
+      if lead.collidepoint(mouse_pos):
+        write(screen, lead, "Leaderboard", 65, "black", "gray69", 10)
+      else:
+        write(screen, lead, "Leaderboard", 65, "black", "white", 10)
+      
+      pygame.display.flip()
+      
+      for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+          pygame.quit()
+          sys.exit()
+        elif event.type == pygame.KEYDOWN:
+          if event.key == pygame.K_ESCAPE:
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+          if start.collidepoint(mouse_pos):
+            starter = False
+          elif lead.collidepoint(mouse_pos):
+            starter = False
+            leaderboard_screen()
+
+  def leaderboard_screen():
+    lead = True
+    while lead:    
+      screen.fill(black)
+      
+      draw("Coming Soon...", 100, height // 2 - 100)
+      
+      home = pygame.Rect(width // 2 - 200, height // 2 + 150, 400, 100)
+      
+      mouse_pos = pygame.mouse.get_pos()
+
+      
+      if home.collidepoint(mouse_pos):
+        write(screen, home, "Return", 65, "black", "gray69", 10)
+      else:
+        write(screen, home, "Return", 65, "black", "white", 10)
+      
+      pygame.display.flip()
+      
+      for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+          pygame.quit()
+          sys.exit()
+        elif event.type == pygame.KEYDOWN:
+          if event.key == pygame.K_ESCAPE:
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+          if home.collidepoint(pygame.mouse.get_pos()):
+            lead = False
+            start_screen()
+  
+  start_screen()
+
+  gaming = True
+
+  pygame.display.flip()
+
+  while gaming:
+    screen.fill(black)
+    
+    pygame.draw.rect(screen, red, (int(spaceship_pos[0]), int(spaceship_pos[1]), 10, 10))
+    
+    v1 += gravity / fps
+    
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_DOWN]:
+      v1 -= a / fps
+    
+    spaceship_pos[1] += v1
+    
+    if keys[pygame.K_LEFT]:
+      v2 += a / (fps * 2)
+    
+    if keys[pygame.K_RIGHT]:
+      v2 -= a / (fps * 2)
+    
+    v2 = max(min(v2, 5.0), -5.0)
+    
+    spaceship_pos[0] += v2
+    
+    pygame.display.flip()
+    clock.tick(fps)
+    
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        pygame.quit()
+        sys.exit()
+      elif event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_ESCAPE:
+          pygame.quit()
+          sys.exit()
+
+main()
